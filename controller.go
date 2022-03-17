3a78befed7ff02164733d933f4dc49643567a197
v_yanffli <v_yanffli@tencent.com>
2022-03-17 16:22:48 +0800

    --other=修改

--- controller.go
+++ controller.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/afengliz/gones/framework"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/afengliz/gones/framework"
+)
 
 func footController(ctx *framework.Context) error {
-	return ctx.Json(200, "Hello liyanfneg")
+	finishChan := make(chan struct{}, 1)
+	panicChan := make(chan interface{}, 1)
+	tCtx, cancelFun := context.WithTimeout(ctx, time.Second)
+	defer cancelFun()
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				panicChan <- r
+			}
+		}()
+		time.Sleep(10 * time.Second)
+		ctx.Json(200, "Hello liyanfneg")
+		finishChan <- struct{}{}
+	}()
+	select {
+	case <-finishChan:
+		fmt.Println("finish")
+	case p := <-panicChan:
+		log.Println(p)
+		ctx.Json(500, "panic")
+	case <-tCtx.Done():
+		ctx.Json(500, "time out")
+		ctx.SetHasTimeOut()
+	}
+	return nil
 }
